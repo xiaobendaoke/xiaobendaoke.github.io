2789094b622f6eda1fad80bac26dee50e481f1d4
xiaobendaoke <2107262208@qq.com>
2025-07-03 18:08:52 +0800

    Site updated: 2025-07-03 18:08:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/11/demo/","2025/07/03/项目/","2025/06/17/论文教程/"];function toRandomPost(){
+var posts=["2025/04/11/demo/","2025/06/17/论文教程/","2025/07/03/项目/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
